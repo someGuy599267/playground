1ecb6b9a15ed85cddec7e88d6fa13714048ce404
randomGuy599267 <lolitshaiech@gmail.com>
2026-05-11 18:40:42 +0330

    changes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import yt_dlp
 import sys
 
 parser = argparse.ArgumentParser(description='')
-parser.add_argument('arg1', help='search query')
+parser.add_argument('query', help='search query')
 args = parser.parse_args()
 
 
@@ -13,9 +13,9 @@ ydl_opts = {
     'extract_flat': True,
     'quiet': True,
 }
-
+print(f'search for {args.query}')
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    res = ydl.extract_info(f'ytsearch20:{args}', download=False)
+    res = ydl.extract_info(f'ytsearch20:{args.query}', download=False)
     for idx ,entry in enumerate(res['entries']):
         print(f'{idx}.')
         print(f'title: {entry["title"]}')
